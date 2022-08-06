896e9dfd440c99b267b74d17734aba963e13b2c6
osvaldo guambe <osvaldofilipeguambe@gmail.com>
2022-08-06 20:06:39 +0200

    criando operacao de instanicacao

--- src/main/java/com/technoplus/cursomc/CursomcApplication.java
+++ src/main/java/com/technoplus/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.technoplus.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.technoplus.cursomc.domain.Categoria;
+import com.technoplus.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null,"informática");
+		Categoria cat2 = new Categoria(null,"informática");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+
 	}
 
 }
