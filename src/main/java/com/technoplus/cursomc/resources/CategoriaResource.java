c76372b62cc8ce595db7832527480080b05719ee
osvaldo guambe <osvaldofilipeguambe@gmail.com>
2022-08-06 15:50:20 +0200

    testando a primeira class de dominio de gategoria

--- src/main/java/com/technoplus/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/technoplus/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.technoplus.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.technoplus.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"informátiaca");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		Categoria cat3 = new Categoria(3,"Sala");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		lista.add(cat3);
+		
+		
+		return lista;
 	}
 
 }
