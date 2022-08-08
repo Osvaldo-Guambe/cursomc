11e6cb5cffa40008aa6f2e245271363aa87623da
osvaldo guambe <osvaldofilipeguambe@gmail.com>
2022-08-08 02:38:40 +0200

    criar controller servico de cliente json ciclico corrigido

---
+++ src/main/java/com/technoplus/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.technoplus.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.technoplus.cursomc.domain.Cliente;
+import com.technoplus.cursomc.repositories.ClienteRepository;
+import com.technoplus.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objecato nao encontradoo"));
+	} 
+	
+
+}
